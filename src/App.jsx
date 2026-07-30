aeadb743b78f5010f2dccef5f615cc7b47ae39a7
mhirrt12 <kaletamene90@gmail.com>
2026-07-30 08:50:15 -0700

    feat: update routing in App component to include correct page elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import Login from "./pages/Login"
-import Login from "./pages/Login"
-import Login from "./pages/Login"
-import Login from "./pages/Login"
+import  products from "./pages/Products"
+import NotFound from "./pages/NotFound"
+import ProductDetails from "./pages/ProductDetails"
+import Dashboared from "./pages/Dashboard"
+import Cart from "./pages/Cart"
 
   function App() {
           
     return (<>
       <Routes>
         <Route path={"/"} element=<Home /> />
-        <Route path={"/login"} element=<Home /> />
-        <Route path={"/products"} element=<Home /> />
-        <Route path={"/cart"} element=<Home /> />
-        <Route path={"/dashboard"} element=<Home /> />
-        <Route path={"/product/:id"} element=<Home /> />
-        <Route path={"*"} element=<Home /> />
+        <Route path={"/login"} element=<Login /> />
+        <Route path={"/products"} element=<products /> />
+        <Route path={"/cart"} element=<Cart /> />
+        <Route path={"/dashboard"} element=<Dashboared /> />
+        <Route path={"/product/:id"} element=<ProductDetails /> />
+        <Route path={"*"} element=<NotFound  /> />
       </Routes>
       <h1>React E-Commerce</h1>
       </>
