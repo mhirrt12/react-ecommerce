6436696ea3541536fdaad3585a39f7cc0bb5a7fa
mhirrt12 <kaletamene90@gmail.com>
2026-07-31 10:19:56 -0700

    fix: update auth reducer reference in store configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from "react-redux"
+import {store} from "./app/store"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+        </Provider>
     </StrictMode>
   </BrowserRouter>
 )
