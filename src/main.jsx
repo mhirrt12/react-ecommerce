b7ee61c9303f44ea3eda586af686092db1bc523e
mhirrt12 <kaletamene90@gmail.com>
2026-07-31 10:24:56 -0700

    refactor: reorganize imports and enhance rendering structure in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from "react-redux"
-import {store} from "./app/store"
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <Provider store={store}>
+import App from "./App";
+import { store } from "./app/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-        </Provider>
-    </StrictMode>
-  </BrowserRouter>
-)
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
