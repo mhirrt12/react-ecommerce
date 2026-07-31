ad7de157d3e73e83e52957e2c4798c9300bbb95b
mhirrt12 <kaletamene90@gmail.com>
2026-07-31 11:31:45 -0700

    feat: add navigation to dashboard on successful login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,16 @@
 import {useForm} from "react-hook-form"
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import dashboard from "./Dashboard"
 const Login = () => {
 
     // React Hook Form here
     const { register, handleSubmit } = useForm();
+    const navigate= useNavigate();
     const dispatch= useDispatch();
     const onSubmit = (data) => {
         dispatch(login(data));
-        
+            navigate("/dashboard")
     };
 
     return (
