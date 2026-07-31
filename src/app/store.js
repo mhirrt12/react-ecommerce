71b670f5ac4d7e1e9967f1b8adf2e3deabf4233a
mhirrt12 <kaletamene90@gmail.com>
2026-07-31 10:31:03 -0700

    refactor: update store export to default and adjust import in main.jsx

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from '../features/auth/authSlice'
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     auth: authSlice,
   },
 })
+export default store;
