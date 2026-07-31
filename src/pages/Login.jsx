e482618585d3fa74a6da639e5c92861a04d44c98
mhirrt12 <kaletamene90@gmail.com>
2026-07-31 11:02:51 -0700

    feat: fix form submission handling in Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import {useForm,handleSubmit} from "react-hook-form"
+import {useForm} from "react-hook-form"
 
 const Login = () => {
 
     // React Hook Form here
+    const { register, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
@@ -10,10 +11,16 @@ const Login = () => {
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit())}>
-                <input type="email" /><br /><br />
-                <input type="password" /><br /><br />
-                <input type="submit" placeholder="submit"/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+               <input
+    type="email"
+    {...register("email")}
+/>
+                <input
+    type="password"
+    {...register("password")}
+/>
+                <input type="submit" placeholder="Login"/>
 
         </form>
         
